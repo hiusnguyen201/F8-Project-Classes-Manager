127ec7f241cc75df6d24beb7a594c065c07dda6b
hiusnguyen201 <hiusnguyen201@gmail.com>
2023-11-23 19:20:58 +0700

    #4 - create migration classes

---
+++ src/models/student_attendance.js
@@ -0,0 +1,33 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Student_Attendance extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Student_Attendance.belongsTo(models.User, { foreignKey: "student_id" });
+      Student_Attendance.belongsTo(models.Class, { foreignKey: "class_id" });
+    }
+  }
+  Student_Attendance.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      date_learning: DataTypes.DATE,
+      status: DataTypes.TINYINT(1),
+      student_id: DataTypes.INTEGER,
+      class_id: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Student_Attendance",
+    }
+  );
+  return Student_Attendance;
+};
---
+++ src/models/student_class.js
@@ -0,0 +1,39 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Student_Class extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Student_Class.belongsTo(models.User, { foreignKey: "student_id" });
+      Student_Class.belongsTo(models.Class, { foreignKey: "class_id" });
+      Student_Class.belongsTo(models.Learning_status, {
+        foreignKey: "status_id",
+      });
+    }
+  }
+  Student_Class.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      student_id: DataTypes.INTEGER,
+      class_id: DataTypes.INTEGER,
+      status_id: DataTypes.INTEGER,
+      reason: DataTypes.STRING(100),
+      complete_date: DataTypes.DATE,
+      dropout_date: DataTypes.DATE,
+      recover_date: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "Student_Class",
+    }
+  );
+  return Student_Class;
+};
