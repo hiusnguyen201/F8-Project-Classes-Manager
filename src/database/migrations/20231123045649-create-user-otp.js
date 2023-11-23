0b6a68cfdff29e147951d5c9ddc7bc2a8faeca14
hiusnguyen201 <hiusnguyen201@gmail.com>
2023-11-23 15:02:13 +0700

    fix migration users

--- src/database/migrations/20231123045649-create-user-otp.js
+++ src/database/migrations/20231123045649-create-user-otp.js
@@ -1,34 +1,35 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('User_Otps', {
+    await queryInterface.createTable("User_Otps", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       otp: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        unique: true,
       },
       expire: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       user_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('User_Otps');
-  }
+    await queryInterface.dropTable("User_Otps");
+  },
 };
