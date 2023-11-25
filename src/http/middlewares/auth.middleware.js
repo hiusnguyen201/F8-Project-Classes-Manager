fd25a8a196debbd6329d860c06668af1fe113b97
Nguyen Minh Hieu <112693497+hiusnguyen201@users.noreply.github.com>
2023-11-25 23:46:12 +0700

    Revert "#16 - Login with Passport and Page Login"

--- src/http/middlewares/auth.middleware.js
+++
@@ -1,8 +0,0 @@
-const { redirectPath } = require("../../constants/constants.path");
-
-module.exports = (req, res, next) => {
-  if (!req.user) {
-    return res.redirect(redirectPath.LOGIN_AUTH);
-  }
-  next();
-};
