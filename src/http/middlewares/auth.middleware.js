a41f77189563a30b05877fb0db74c1e56dedeac8
hiusnguyen201 <hiusnguyen201@gmail.com>
2023-11-25 23:45:17 +0700

    #16 - Login with Passport and Page Login

---
+++ src/http/middlewares/auth.middleware.js
@@ -0,0 +1,8 @@
+const { redirectPath } = require("../../constants/constants.path");
+
+module.exports = (req, res, next) => {
+  if (!req.user) {
+    return res.redirect(redirectPath.LOGIN_AUTH);
+  }
+  next();
+};
